cf55d58600c7ecb7f0321f9a39db67900b8d737a
Dzmitry Mukhliada <33730148+N0menIllisLegio@users.noreply.github.com>
2021-03-06 01:33:14 +0300

    Initialize project
    Add Prism

---
+++ ci-cd/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace ci_cd
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : PrismApplication
+  {
+    protected override Window CreateShell()
+    {
+      return Container.Resolve<MainWindow>();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    { }
+  }
+}
